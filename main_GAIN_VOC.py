Fix bugs in accuracy calculation and set picked_categories in main_GAIN_VOC and passcal_voc_loader as aeroplane

main_GAIN_VOC.py
@@ -43,7 +43,6 @@ parser.add_argument('--npretrain', type=int, help='number of epochs to pretrain 
 parser.add_argument('--record_itr_train', type=int, help='each which number of iterations to log images in training mode', default=1000)
 parser.add_argument('--record_itr_test', type=int, help='each which number of iterations to log images in test mode', default=100)
 parser.add_argument('--nrecord', type=int, help='how much images of a batch to record', default=1)
-parser.add_argument('--pickedCategories', type=list, help='a list of category number to train and test on')
 
 parser.add_argument('--grads_off', type=int, default=0, help='mode: \
                 with gradients (as in the paper) or without \
@@ -62,7 +61,7 @@ parser.add_argument('--grads_magnitude', type=int, default=1,
 def main(args):
     categories = cfg.CATEGORIES
     num_classes = len(categories)
-
+    picked_categories = ['aeroplane']
     device = torch.device(args.device)
     model = vgg19(pretrained=True).train().to(device)
 
@@ -159,7 +158,7 @@ def main(args):
             for sample in rds.datasets['rnd_train']:
                 # TODO:
                 augmented_masks = sample[3]
-                mask_flag = sample[4]
+                mask_flag = sample[4] # data.py my_collate mask_flags
 
                 # Get batch and augmented batch for the 1st img in the 1st array of sample
                 augmented_batch = []
@@ -216,6 +215,7 @@ def main(args):
                 # augmented_masks = torch.cat(augmented_masks, dim=0)
                 # train_dataset -> medt_loader.datasets['train']
                 e_loss = 0.0
+                print(mask_flag[0])
                 if mask_flag[0]:
                     print("***********External supervision added***********")
                     print(sample[5])
@@ -268,13 +268,19 @@ def main(args):
                     acc = (y_pred == gt).sum()
                     correct_label_counter = 0
                     total_picked_gt_label = 0
-                    if args.pickedCategories:
+                    if picked_categories:
                         for gt_label in gt:
-                            if gt_label in args.pickedCategories:
+                            if gt_label in picked_categories:
                                 total_picked_gt_label += 1
-                            if gt_label in y_pred and gt_label in args.pickedCategories:
+                            if gt_label in y_pred and gt_label in picked_categories:
                                 correct_label_counter += 1
-                        total_train_single_accuracy += (correct_label_counter / correct_label_counter)
+                        if total_picked_gt_label == 0:
+                            if correct_label_counter != 0:
+                                pass
+                            else:
+                                total_train_single_accuracy += 1
+                        else:
+                            total_train_single_accuracy += (correct_label_counter / total_picked_gt_label)
                     else:
                         acc = acc.detach().cpu()/len(gt)
                         total_train_single_accuracy += acc
@@ -370,6 +376,8 @@ def main(args):
 
 
             # Single label evaluation
+
+
             for k in range(len(batch)):
                 num_of_labels = len(sample[2][k])
                 _, y_pred = logits_cl[k].detach().topk(k=num_of_labels)
@@ -378,6 +386,28 @@ def main(args):
 
                 acc = (y_pred == gt).sum()
                 total_test_single_accuracy += acc.detach().cpu()
+
+                correct_label_counter = 0
+                total_picked_gt_label = 0
+                if picked_categories:
+                    for gt_label in gt:
+                        if gt_label in picked_categories:
+                            total_picked_gt_label += 1
+                        if gt_label in y_pred and gt_label in picked_categories:
+                            correct_label_counter += 1
+                    if total_picked_gt_label == 0:
+                        if correct_label_counter != 0:
+                            pass
+                        else:
+                            total_test_single_accuracy += 1
+                    else:
+                        total_test_single_accuracy += (correct_label_counter / total_picked_gt_label)
+                else:
+                    acc = acc.detach().cpu() / len(gt)
+                    total_test_single_accuracy += acc
+                    if acc.detach().cpu() > 1:
+                        print("Invalid acc")
+                        print(acc.detach().cpu())
 
             if j % args.record_itr_test == 0:
                 for t in range(args.nrecord):

models/batch_GAIN_MedT.py
@@ -144,7 +144,7 @@ class batch_GAIN_MedT(nn.Module):
         Ac = torch.mul(fl, weights).sum(dim=1, keepdim=True)
         Ac = F.relu(Ac)
         # Ac = F.interpolate(Ac, size=images.size()[2:], mode='bilinear', align_corners=False)
-        Ac = F.upsample_bilinear(Ac, size=images.size()[2:])
+        Ac = F.interpolate(Ac, size=images.size()[2:], mode='bilinear')
 
         Ac_min, _ = Ac.view(len(images), -1).min(dim=1)
         Ac_max, _ = Ac.view(len(images), -1).max(dim=1)
@@ -153,7 +153,7 @@ class batch_GAIN_MedT(nn.Module):
         scaled_ac = (Ac - Ac_min.view(-1, 1, 1, 1)) / \
                     (Ac_max.view(-1, 1, 1, 1) - Ac_min.view(-1, 1, 1, 1)
                      + eps.view(1, 1, 1, 1))
-        mask = F.sigmoid(self.omega * (scaled_ac - self.sigma))
+        mask = torch.sigmoid(self.omega * (scaled_ac - self.sigma))
 
         masked_image = images - images * mask + mask * self.fill_color
 

models/batch_GAIN_VOC.py
@@ -158,7 +158,7 @@ class batch_GAIN_VOC(nn.Module):
         Ac = torch.mul(fl, weights).sum(dim=1, keepdim=True)
         Ac = F.relu(Ac)
         # Ac = F.interpolate(Ac, size=images.size()[2:], mode='bilinear', align_corners=False)
-        Ac = F.upsample_bilinear(Ac, size=images.size()[2:])
+        Ac = F.interpolate(Ac, size=images.size()[2:], mode='bilinear')
         heatmap = Ac
 
         Ac_min, _ = Ac.view(len(images), -1).min(dim=1)
@@ -168,7 +168,7 @@ class batch_GAIN_VOC(nn.Module):
         scaled_ac = (Ac - Ac_min.view(-1, 1, 1, 1)) / \
                     (Ac_max.view(-1, 1, 1, 1) - Ac_min.view(-1, 1, 1, 1)
                      + eps.view(1, 1, 1, 1))
-        mask = F.sigmoid(self.omega * (scaled_ac - self.sigma))
+        mask = torch.sigmoid(self.omega * (scaled_ac - self.sigma))
         masked_image = images - images * mask
         
         #masked_image.register_hook(lambda grad: grad * self.grad_magnitude)