e548ac14749735d1d402e3f2b706bfb5f07228e8
TwistedAlex <muzihuole@gmail.com>
2022-09-08 13:24:19 -0500

    add em loss

--- models/batch_GAIN_Deepfake.py
+++ models/batch_GAIN_Deepfake.py
@@ -195,18 +195,19 @@ class batch_GAIN_Deepfake(nn.Module):
             # em_masked_image size [2, 3, 224, 224]
             em_masked_image = image_with_masks * em_mask + (torch.ones(em_mask.shape).to(torch.device('cuda:' + str(0)))
                                                             - em_mask) * self.em_fill_color
-
-            # PIL.Image.fromarray((image_with_masks[0].permute([1, 2, 0]).cpu().detach().numpy() * 255).round().astype(
-            #     np.uint8), 'RGB').save('/home/shuoli/image.png')
+            import PIL.Image
+            import numpy as np
+            PIL.Image.fromarray((image_with_masks[0].permute([1, 2, 0]).cpu().detach().numpy() * 255).round().astype(
+                np.uint8), 'RGB').save('/home/shuoli/image.png')
             # PIL.Image.fromarray(((image_with_masks * em_mask)[0].cpu().detach().numpy() * 255).round().astype(
             #     np.uint8), 'RGB').save('/home/shuoli/image_times_mask.png')
             # PIL.Image.fromarray(((self.fill_color * em_mask)[0].cpu().detach().numpy() * 255).round().astype(
             #     np.uint8), 'RGB').save('/home/shuoli/fill_times_mask.png')
             # PIL.Image.fromarray(((em_mask[0]).cpu().detach().numpy() * 255).round().astype(
             #     np.uint8), 'RGB').save('/home/shuoli/em_mask.png')
-            # PIL.Image.fromarray((em_masked_image[0].permute([1, 2, 0]).cpu().detach().numpy() * 255)
-            #                     .round().astype(np.uint8), 'RGB').save('/home/shuoli/masked.png')
-
+            PIL.Image.fromarray((em_masked_image[0].permute([1, 2, 0]).cpu().detach().numpy() * 255)
+                                .round().astype(np.uint8), 'RGB').save('/home/shuoli/masked.png')
+            exit(1)
             logits_em = self.model(em_masked_image)  # [2, 1]
 
             # for param in self.model.parameters(): #TODO: use this to control set gradients on/off
