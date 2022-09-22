b742db105f3508f3df5b76701c81daaf21cc868e
TwistedAlex <muzihuole@gmail.com>
2022-09-22 14:41:01 -0500

    rearrange preprocessing module

--- utils/image.py
+++ utils/image.py
@@ -436,12 +436,10 @@ def deepfake_preprocess_imagev2(img , train , mask=-1, mean=None, std=None) -> t
 
     if train == True:
         augment = Compose([
-            RandomResizedCrop(224, scale=(0.9, 1.0), ratio=(0.9, 1.1)),
+            RandomResizedCrop(224, scale=(0.6, 1.0), ratio=(0.9, 1.1)),
             transforms.RandomApply([
-
                 RandomRotation((-180, 180)),
-            ], p=0.5)
-
+            ], p=0.5),
         ])
         normilize_augment = Compose([
             ToTensor(),
