b8f23f357c1f394bfd6e7b269e36b1134bc69122
TwistedAlex <muzihuole@gmail.com>
2022-10-03 21:37:31 -0500

    update new testing dataset

--- main_cnn_with_GAIN.py
+++ main_cnn_with_GAIN.py
@@ -662,7 +662,7 @@ def train(args, cfg, model, device, train_loader, train_dataset, optimizer,
                     np.uint8), 'RGB').save("masked_em3.png")
             PIL.Image.fromarray(
                 (e_masks[0].permute([1, 2, 0]).cpu().detach().numpy() * 255).round().astype(
-                    np.uint8), 'RGB').save("masked_em3.png")
+                    np.uint8), 'L').save("masked_em3.png")
             exit(0)
         iter_em_flag = args.train_with_em and has_mask_flag
 
